380dcf76cbd77af2b9711a9031ffd6df4a52260e
Steve Váradi <varadiista.1@gmail.com>
2025-12-04 10:42:43 +0100

    Day 4
    This day was fairly easy, given that I don't confuse a zip with a
    cartesian product. I kind of thought that part 2 was going to be part 1
    while reading the instruction, and that part 2 was going to be something
    brutal after it
    
    Signed-off-by: Steve Váradi <varadiista.1@gmail.com>

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,7 @@
+use std::hint::black_box;
+
+use itertools::Itertools;
+
 #[cfg(debug_assertions)]
 const fn get_file() -> &'static str {
     include_str!("../input2.txt")
@@ -11,15 +15,82 @@ const fn get_file() -> &'static str {
 fn main() {
     const FILE: &str = get_file();
     println!("Part 1: {}", part_1(FILE));
-    // println!("Part 2: {}", part_2(FILE));
+    println!("Part 2: {}", part_2(FILE));
 }
 
 fn part_1(file: &str) -> usize {
-    todo!()
+    let mut inp: Vec<Vec<_>> = file.lines().map(|l| l.chars().collect()).collect();
+    let width = inp[0].len();
+    let height = inp.len();
+    let mut removed = 0;
+
+    for y in 0..inp.len() {
+        let line_len = inp[y].len();
+        for x in 0..line_len {
+            let char = inp[y][x];
+            if char == '@' {
+                let neighbours = (if y == 0 { 0 } else { -1 }..if y == height - 1 { 1 } else { 2 })
+                    .cartesian_product(
+                        if x == 0 { 0 } else { -1 }..if x == width - 1 { 1 } else { 2 },
+                    )
+                    .map(|(check_y, check_x)| unsafe {
+                        let c = inp[y.checked_add_signed(check_y).unwrap_unchecked()]
+                            [x.checked_add_signed(check_x).unwrap_unchecked()];
+                        black_box(&c);
+                        c
+                    })
+                    .filter(|c| *c == '@' || *c == 'x')
+                    .count();
+
+                if neighbours < 5 {
+                    inp[y][x] = 'x';
+                    removed += 1;
+                }
+            }
+        }
+    }
+
+    removed
 }
 
 fn part_2(file: &str) -> usize {
-    todo!()
+    let mut inp: Vec<Vec<_>> = file.lines().map(|l| l.chars().collect()).collect();
+    let width = inp[0].len();
+    let height = inp.len();
+    let mut changed = true;
+    let mut removed = 0;
+    while changed {
+        changed = false;
+        for y in 0..inp.len() {
+            let line_len = inp[y].len();
+            for x in 0..line_len {
+                let char = inp[y][x];
+                if char == '@' {
+                    let neighbours =
+                        (if y == 0 { 0 } else { -1 }..if y == height - 1 { 1 } else { 2 })
+                            .cartesian_product(
+                                if x == 0 { 0 } else { -1 }..if x == width - 1 { 1 } else { 2 },
+                            )
+                            .map(|(check_y, check_x)| unsafe {
+                                let c = inp[y.checked_add_signed(check_y).unwrap_unchecked()]
+                                    [x.checked_add_signed(check_x).unwrap_unchecked()];
+                                black_box(&c);
+                                c
+                            })
+                            .filter(|c| *c == '@')
+                            .count();
+
+                    if neighbours < 5 {
+                        inp[y][x] = 'x';
+                        changed = true;
+                        removed += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    removed
 }
 
 mod test {
