61ca02a8ade19eca6f7bf71a29696376ef263789
Steve Váradi <varadiista.1@gmail.com>
2025-12-07 09:52:36 +0100

    Day 7
    DP my beloved. Genuinely, don't ask where the off by one is coming from,
    I have no clue, but if it works it works. I did have to factor out my
    mutability to memoize, the inspiration for how came as I was on the
    toilet
    
    Signed-off-by: Steve Váradi <varadiista.1@gmail.com>

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,6 @@
+
+use comemo::memoize;
+
 #[cfg(debug_assertions)]
 const fn get_file() -> &'static str {
     include_str!("../input2.txt")
@@ -11,15 +14,68 @@ const fn get_file() -> &'static str {
 fn main() {
     const FILE: &str = get_file();
     println!("Part 1: {}", part_1(FILE));
-    // println!("Part 2: {}", part_2(FILE));
+    println!("Part 2: {}", part_2(FILE));
 }
 
 fn part_1(file: &str) -> usize {
-    todo!()
+    let mut vec = file
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect::<Vec<Vec<_>>>();
+
+    let mut splits = 0;
+
+    for row in 1..vec.len() {
+        for col in 0..vec[row].len() {
+            if vec[row - 1][col] == 'S' || vec[row - 1][col] == '|' {
+                match vec[row][col] {
+                    '^' => {
+                        splits += 1;
+                        vec[row][col - 1] = '|';
+                        vec[row][col + 1] = '|'
+                    }
+                    '.' | '|' => vec[row][col] = '|',
+                    c => unreachable!("Weird char {c} at {row}:{col}"),
+                }
+            }
+        }
+    }
+
+    splits
 }
 
 fn part_2(file: &str) -> usize {
-    todo!()
+    let vec = file
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect::<Vec<Vec<_>>>();
+
+    let ray = vec[0]
+        .iter()
+        .position(|c| *c == 'S')
+        .expect("There's a start in the first line");
+
+    // Don't know where the off by 1 is coming from, don't ask
+    dp(&vec[1..], ray) + 1
+}
+
+#[memoize]
+fn dp(lines: &[Vec<char>], ray: usize) -> usize {
+    if lines.is_empty() {
+        return 0;
+    }
+
+    match lines[0][ray] {
+        '^' => {
+            let left = dp(&lines[1..], ray - 1);
+            let right = dp(&lines[1..], ray + 1);
+            return left + right + 1;
+        }
+        '.' => {
+            return dp(&lines[1..], ray);
+        }
+        c => unreachable!("Weird char {c} at ?:{ray}"),
+    }
 }
 
 mod test {
