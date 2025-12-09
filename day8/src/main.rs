84a92e460a1bbc8202483a0219acb79b9d9ed971
Steve Váradi <varadiista.1@gmail.com>
2025-12-09 01:38:41 +0100

    Use a disjoint set for day 8
    Signed-off-by: Steve Váradi <varadiista.1@gmail.com>

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,7 +1,6 @@
 use std::fmt;
 use std::{cmp::Reverse, collections::BinaryHeap};
 
-
 #[cfg(debug_assertions)]
 const fn get_file() -> &'static str {
     include_str!("../input2.txt")
@@ -134,50 +133,26 @@ fn part_2(file: &str) -> usize {
         for j in (i + 1)..points.len() {
             let dist = points[i].dist(&points[j]);
             let ordered_dist = ordered_float::NotNan::new(dist).unwrap();
-            thing_vec.push(Reverse((ordered_dist, &points[i], &points[j])));
+            thing_vec.push(Reverse((ordered_dist, i, j)));
         }
     }
 
     let mut thing = BinaryHeap::from(thing_vec);
 
-    let mut clusters: Vec<Vec<&Point>> = Vec::with_capacity(1000);
+    let mut clusters = disjoint::DisjointSet::with_len(point_count);
 
-    while clusters.iter().map(|c| c.len()).sum::<usize>() < point_count || clusters.len() > 1 {
+    let mut num_clust = point_count;
+
+    while num_clust > 1 {
         thing.pop().unwrap();
         let Reverse(next_pair) = thing.peek().unwrap();
-        let mut found_clusters = clusters
-            .iter_mut()
-            .enumerate()
-            .filter(|c| c.1.contains(&next_pair.1) || c.1.contains(&next_pair.2))
-            .collect::<Vec<_>>();
-        debug_assert!(found_clusters.len() < 3);
-        match found_clusters.len() {
-            0 => clusters.push(Vec::from([next_pair.1, next_pair.2])),
-            1 => {
-                if !(found_clusters[0].1.contains(&next_pair.1)
-                    && found_clusters[0].1.contains(&next_pair.2))
-                {
-                    match found_clusters[0].1.contains(&next_pair.1) {
-                        true => found_clusters[0].1.push(next_pair.2),
-                        false => found_clusters[0].1.push(next_pair.1),
-                    }
-                }
-            }
-            2 => {
-                let mut bruh = found_clusters.into_iter();
-                let c1 = bruh.next().unwrap().1;
-                let (i2, c2) = bruh.next().unwrap();
-                c1.append(c2);
-                clusters.remove(i2);
-            }
-            _ => {
-                unreachable!("Two points should never be in more than 2 clusters");
-            }
+        if clusters.join(next_pair.1, next_pair.2) {
+            num_clust -= 1;
         }
     }
 
     let Reverse(last_thing) = thing.pop().unwrap();
-    (last_thing.1.x * last_thing.2.x) as usize
+    (points[last_thing.1].x * points[last_thing.2].x) as usize
 }
 
 mod test {
